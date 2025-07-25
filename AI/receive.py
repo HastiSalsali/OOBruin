d29699003e3b77982e24764fd3837a6fe08c0ffe
Hasti <hastisalsali@gmail.com>
2025-07-25 16:40:49 -0700

    redid page + added some of the ai but its not working

--- AI/receive.py
+++ AI/receive.py
@@ -1,12 +1,14 @@
 
 # TODO: import your module
+from openai import OpenAI
 import requests
 import os
 import sys
+from secrets import API_KEY
 
 # Get the folder where the script is located, done for you
 script_dir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(script_dir, "downloaded_image.jpg")
+filename = os.path.join(script_dir, "../frontend/src/downloaded_image.jpg")
 
 url = "http://172.20.10.6/1024x768.jpg"             # You will have to change the IP Address
 
@@ -25,3 +27,21 @@ def download_image():
 
 # TODO: How to control when to take photo?
 
+
+client = OpenAI(api_key = API_KEY)
+
+response = client.responses.create(
+    model="gpt-4.1-mini",
+    input=[{
+        "role": "user",
+        "content": [
+            {"type": "input_text", "text": "what's in this image?"},
+            {
+                "type": "input_image",
+                "image_url": "",
+            },
+        ],
+    }],
+)
+
+print(response.output_text)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,92 @@ import React, { useState, useEffect } from "react";
 import io from 'socket.io-client';
 import './App.css';
 import { Header } from "./header";
-import { Measure } from "./Measure";
-import { Camera } from "./Camera";
 
 const socket = io('http://localhost:8000');
 
 function App() {
   const [pictureStatus, setPictureStatus] = useState("");
+  const [pictureAvail, setPictureAvail] = useState(false)
+
 
   useEffect(() => {
     socket.on('connect', () => console.log('Connected:', socket.id));
     socket.on('picture_taken', data => {
       setPictureStatus(data.message);
+
       setTimeout(() => setPictureStatus(""), 3000); // Clear status after 3 seconds
     });
+    console.log('STARTED PROGRAM')
     return () => {
       socket.off('picture_taken');
     };
   }, []);
 
+
+  
+
+   
+
+  //measurements:
+  const [tempRes, settempRes] = useState("No measurements done yet!");
+  const [humidRes, sethumidRes] = useState("No measurements done yet!");
+  const [lightRes, setlightRes] = useState("No measurements done yet!");
+  const [distRes, setdistRes] = useState("No measurements done yet!");
+
+  socket.on('temp', (latestTemp) => {
+    settempRes(latestTemp);
+    //console.log("got temp measurements" + latestTemp)
+  })
+  socket.on('humidity', (latestHumidity) => {
+    sethumidRes(latestHumidity);
+    //console.log("got Humidity measurements" + latestHumidity);
+  })
+  socket.on('light', (latestLight) => {
+    setlightRes(latestLight);
+    //console.log("got light measurements" + latestLight);
+  })
+  socket.on('ultrasonic', (latestUltrasonic) => {
+    setdistRes(latestUltrasonic);
+    //console.log("got distance measurements" + latestUltrasonic);
+  })
+
+  //Take photo
+
+  function Camera(prop) {
+    let handleClick = () => {
+      socket.emit('take_picture')
+          console.log('taking photo')
+
+    }
+    return (
+      <div style={{ padding: "24px", borderRadius: "8px", maxWidth: "400px", margin: "24px auto" }}>
+        <h2 className="Heading1">Camera:</h2>
+        <button onClick={handleClick} className="Button">Take photo</button >
+        {prop.pictureAvail ? (
+          <div style={{ marginTop: "16px" }}>
+            <img src="/downloaded_image.jpg" alt="Taken" style={{ width: "100%", borderRadius: "8px" }} />
+          </div>
+        ) :
+          (<div className="Result">No image captured yet</div>)}
+      </div>
+    );
+  }
+
+
+  ////////
   return (
     <div className="app">
       <Header />
-      <Measure />
-      <Camera />
+      <div style={{ padding: "24px", borderRadius: "8px", maxWidth: "400px", margin: "24px auto" }}>
+        <h2 className="Heading1">Measurements:</h2>
+        <p className="Result"> Temp value: {tempRes ? tempRes : "No measurements done yet!"} </p>
+        <p className="Result"> Humidity value: {humidRes ? humidRes : "No measurements done yet!"} </p>
+        <p className="Result"> Light value: {lightRes ? lightRes : "No measurements done yet!"} </p>
+        <p className="Result"> Distance value: {distRes ? distRes : "No measurements done yet!"} </p>
+      </div>
+      <Camera pictureAvail = {pictureAvail}/>
     </div>
+
   );
 }
 
