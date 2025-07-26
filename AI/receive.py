cea71623b28cd44e4d9c7b2ddc25943cf5e58a54
Hasti <hastisalsali@gmail.com>
2025-07-26 14:51:28 -0700

    web gets photo from camera, and ai describes it with audio. -the audio doesnt upload to site without refreshin though

--- AI/receive.py
+++ AI/receive.py
@@ -11,8 +11,10 @@ from my_secrets import API_KEY
 script_dir = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(script_dir, "../frontend/public/downloaded_image.jpg")
 
-#url = "https://canto-wp-media.s3.amazonaws.com/app/uploads/2019/08/19194139/image-url.jpg"             # You will have to change the IP Address
-url = "https://www.movies4kids.co.uk/wp-content/uploads/sites/15/2019/04/spycat.jpg"
+#url = "https://canto-wp-media.s3.amazonaws.com/app/uploads/2019/08/19194139/image-url.jpg" 
+#url = "https://www.movies4kids.co.uk/wp-content/uploads/sites/15/2019/04/spycat.jpg"
+url = "http://192.168.50.72/1600x1200.jpg"
+
 # Function to download the image from esp32, given to you
 def download_image():
     response = requests.get(url)
@@ -31,6 +33,19 @@ def download_image():
 download_image()
 client = OpenAI(api_key = API_KEY)
 
+
+# Function to create a file with the Files API
+def create_file(file_path):
+  with open(file_path, "rb") as file_content:
+    result = client.files.create(
+        file=file_content,
+        purpose="vision",
+    )
+    return result.id
+
+# Getting the file ID
+file_id = create_file("../frontend/public/downloaded_image.jpg")
+
 response = client.responses.create(
     model="gpt-4.1-mini",
     input=[{
@@ -39,7 +54,7 @@ response = client.responses.create(
             {"type": "input_text", "text": "what's in this image?"},
             {
                 "type": "input_image",
-                "image_url": url,
+                "file_id": file_id,
             },
         ],
     }],
@@ -47,12 +62,13 @@ response = client.responses.create(
 
 print(response.output_text)
 
+
 speech_file_path = os.path.join(script_dir, "../frontend/public/image_to_speach.mp3") 
 
 with client.audio.speech.with_streaming_response.create(
     model="gpt-4o-mini-tts",
-    voice="coral",
+    voice="ash",
     input= response.output_text,
-    instructions="Speak like you are a spy on a secret mission",
+    instructions="Speak like a spy like james bond. have a british accent",
 ) as response:
     response.stream_to_file(speech_file_path)
